2a65c70179b044e35fcc3ad584fe61ecf68a1c8a
essential-workshop-9972 <josephjones7@gmail.com>
2020-01-27 07:58:51 +0000

    Automatically backed up by Learn

---
+++ aoh_lab.rb
@@ -0,0 +1,20 @@
+director_1 = {
+  name: "Ida Lupino",
+  favorite_movie: "The Hitch-Hiker"
+}
+ 
+director_2 = {
+  name: "Orson Welles",
+  favorite_movie: "Citizen Kane"
+}
+ 
+director_3 = {
+  name: "Andrej Tarkovsky",
+  favorite_movie: "Solaris"
+}
+ 
+film_fest = [
+  director_1,
+  director_2,
+  director_3,
+]
